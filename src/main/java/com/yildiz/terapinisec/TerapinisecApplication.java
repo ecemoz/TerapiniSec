ddcde4820228d160c70c3aa57d591960aa31581e
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-15 00:09:25 +0300

    application packages is created.

---
+++ src/main/java/com/yildiz/terapinisec/TerapinisecApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.terapinisec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerapinisecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerapinisecApplication.class, args);
+	}
+
+}
