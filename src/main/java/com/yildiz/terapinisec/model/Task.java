bb0dfc14cff3a99be9f9e91bb4f4641fe070f789
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-25 16:53:25 +0300

    task model finished.

--- src/main/java/com/yildiz/terapinisec/model/Task.java
+++ src/main/java/com/yildiz/terapinisec/model/Task.java
@@ -1,4 +1,33 @@
 package com.yildiz.terapinisec.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(nullable = false)
+    private String taskName ;
+
+    @Column(nullable = false)
+    private String taskDescription ;
+
+    @Column(nullable = false)
+    private LocalDateTime dueDate;
+
+    @Column(nullable = false)
+    private Boolean iscompleted = false ;
+
+    @ManyToOne
+    @JoinColumn(name= "assignees")
+    private User assignees ;
 }
