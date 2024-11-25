dbba28bd782a4e4f66649fb181ba158e928e4ba0
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-25 17:32:20 +0300

    story model finished.

--- src/main/java/com/yildiz/terapinisec/model/Story.java
+++ src/main/java/com/yildiz/terapinisec/model/Story.java
@@ -1,4 +1,38 @@
 package com.yildiz.terapinisec.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "stories")
 public class Story {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String storyUrl ;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime storyCreatedAt;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    @PrePersist
+    protected void onCreate() {
+        storyCreatedAt = LocalDateTime.now();
+    }
+
+    @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StoryView> storyViews;
 }
