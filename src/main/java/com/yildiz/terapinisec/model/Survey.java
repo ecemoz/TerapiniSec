c5308382de29b85381dba746b2c53475b7b5f300
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-25 20:23:32 +0300

    survey model finished.

--- src/main/java/com/yildiz/terapinisec/model/Survey.java
+++ src/main/java/com/yildiz/terapinisec/model/Survey.java
@@ -1,4 +1,35 @@
 package com.yildiz.terapinisec.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "surveys")
 public class Survey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false ,length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime surveyCreatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.surveyCreatedAt = LocalDateTime.now();
+    }
+
+    @OneToMany(mappedBy = "survey" , fetch = FetchType.LAZY)
+    private List<SurveyResponse> surveyResponses;
 }
