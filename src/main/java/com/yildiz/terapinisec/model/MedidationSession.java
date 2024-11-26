9c0f884d4b59875f76adcbdae527efc757095c4e
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-26 19:17:15 +0300

    meditation session model finished.

--- src/main/java/com/yildiz/terapinisec/model/MedidationSession.java
+++ src/main/java/com/yildiz/terapinisec/model/MedidationSession.java
@@ -1,4 +1,33 @@
 package com.yildiz.terapinisec.model;
 
+import com.yildiz.terapinisec.util.MedidationSessionType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "meditationsessions")
 public class MedidationSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MedidationSessionType medidationSessionType;
+
+    @Column(nullable = false)
+    private Duration medidationSessionDuration;
+
+    @Column(nullable = false)
+    private LocalDateTime medidationDateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User meditator;
 }
---
+++ src/main/java/com/yildiz/terapinisec/util/MedidationSessionType.java
@@ -0,0 +1,17 @@
+package com.yildiz.terapinisec.util;
+
+public enum MedidationSessionType {
+    GUIDED_MEDITATION,       // Rehberli meditasyon
+    MINDFULNESS,             // Farkındalık meditasyonu
+    TRANSCENDENTAL,          // Transandantal meditasyon
+    RELAXATION,              // Rahatlama odaklı meditasyon
+    SPIRITUAL,               // Spiritüel meditasyon
+    MOVEMENT_BASED,          // Hareket tabanlı meditasyon (örneğin yoga)
+    FOCUSED_ATTENTION,       // Odaklanma meditasyonu
+    BODY_SCAN,               // Beden tarama meditasyonu
+    BREATHING_EXERCISE,      // Nefes egzersizi meditasyonu
+    SOUND_HEALING,           // Ses terapisi meditasyonu
+    LOVING_KINDNESS,         // Sevgi dolu nezaket meditasyonu
+    ZEN,                     // Zen meditasyonu
+    MANTRA                   // Mantra meditasyonu
+}
