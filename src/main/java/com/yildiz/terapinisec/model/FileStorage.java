58f8cb642b3d9f72596c4153fd8ebd16ca30868a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-27 20:47:36 +0300

    file storage model fnished.

--- src/main/java/com/yildiz/terapinisec/model/FileStorage.java
+++ src/main/java/com/yildiz/terapinisec/model/FileStorage.java
@@ -1,4 +1,46 @@
 package com.yildiz.terapinisec.model;
 
+import com.yildiz.terapinisec.util.FileType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "filestorage")
 public class FileStorage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(unique = true, nullable = false)
+    private String fileName ;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private FileType fileType ;
+
+    @Column(nullable = false,unique = true)
+    private String fileUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime fileUploadDate ;
+
+    @PrePersist
+    protected void onCreate() {
+        fileUploadDate = LocalDateTime.now();
+    }
+
+    @Column(nullable = false)
+    private float fileSize;
+
+    @Column(nullable = false)
+    private boolean isFilePublic = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User documentUploader;
 }
---
+++ src/main/java/com/yildiz/terapinisec/util/FileType.java
@@ -0,0 +1,14 @@
+package com.yildiz.terapinisec.util;
+
+public enum FileType {
+    PDF,               // PDF dosyaları
+    WORD,              // Microsoft Word dosyaları
+    EXCEL,             // Microsoft Excel dosyaları
+    IMAGE,             // JPEG, PNG gibi görseller
+    VIDEO,             // MP4, AVI gibi video dosyaları
+    AUDIO,             // MP3, WAV gibi ses dosyaları
+    TEXT,              // TXT dosyaları
+    ZIP,               // ZIP veya RAR dosyaları
+    PRESENTATION,      // PowerPoint gibi sunum dosyaları
+    OTHER              // Diğer dosya türleri
+}
