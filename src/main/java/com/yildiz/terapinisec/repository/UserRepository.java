b0f2540986328f0293246f2fba4d770349f3ee8d
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-27 21:30:46 +0300

    UserRepository finished.

--- src/main/java/com/yildiz/terapinisec/repository/UserRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/UserRepository.java
@@ -1,4 +1,24 @@
 package com.yildiz.terapinisec.repository;
 
-public class UserRepository {
+import com.yildiz.terapinisec.model.User;
+import com.yildiz.terapinisec.util.Specialization;
+import com.yildiz.terapinisec.util.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface  UserRepository  extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByFirstNameAndLastName(String firstName, String lastName);
+    User findByEmail(String email);
+    User findByPhoneNumber(String phoneNumber);
+    User findByRole(UserRole role);
+    User findByLastLoginDateTimeBefore(LocalDateTime dateTime);
+    User findByLastLoginDateTimeAfter(LocalDateTime dateTime);
+    User findByIsPremiumTrue();
+    User findByIsPremiumFalse();
+    User findBySpecializationContains(Specialization specialization);
+    User findByYearsOfExperienceGreaterThan(Integer years);
 }
