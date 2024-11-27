24669d94b0f0674e18d240e2d757a8f88744010a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-27 19:32:32 +0300

    library document model finished.

--- src/main/java/com/yildiz/terapinisec/model/LibraryDocument.java
+++ src/main/java/com/yildiz/terapinisec/model/LibraryDocument.java
@@ -1,4 +1,41 @@
 package com.yildiz.terapinisec.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "librarydocuments")
 public class LibraryDocument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(unique = true, nullable = false)
+    private String title ;
+
+    @Column(unique = true, nullable = false)
+    private String description ;
+
+    @Column(nullable = false)
+    private String documentUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime documentUploadDate;
+
+    @PrePersist
+    protected void onCreate() {
+        documentUploadDate = LocalDateTime.now();
+    }
+
+    @Column(nullable = false)
+    private boolean isPublic = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "user_id",nullable = false)
+    private User fileUploader;
 }
