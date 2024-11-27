39730d0dbd445ff5efe5c5de6afc84c5f07f60af
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-27 20:26:53 +0300

    notification model finished

--- src/main/java/com/yildiz/terapinisec/model/Notification.java
+++ src/main/java/com/yildiz/terapinisec/model/Notification.java
@@ -1,4 +1,40 @@
 package com.yildiz.terapinisec.model;
 
+import com.yildiz.terapinisec.util.NotificationType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "notifications")
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private boolean isRead = false;
+
+    @Column(nullable = false)
+    private LocalDateTime notiCreatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.notiCreatedAt = LocalDateTime.now();
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User notiReceiver;
 }
---
+++ src/main/java/com/yildiz/terapinisec/util/NotificationType.java
@@ -0,0 +1,16 @@
+package com.yildiz.terapinisec.util;
+
+public enum NotificationType {
+
+    APPOINTMENT_REMINDER,     // Randevu hatırlatması
+    NEW_MESSAGE,              // Yeni mesaj bildirimi
+    TASK_REMINDER,            // Görev hatırlatması
+    SYSTEM_ALERT,             // Sistem uyarısı
+    PROMOTION,                // Promosyon veya kampanya bildirimi
+    MOOD_TRACKING_ALERT,      // Ruh hali takibi bildirimi
+    WEEKLY_REPORT,            // Haftalık rapor bildirimi
+    THERAPIST_FEEDBACK,       // Terapist geri bildirimi
+    SURVEY_INVITATION,        // Anket daveti bildirimi
+    GROUP_SESSION_REMINDER,   // Grup terapi hatırlatması
+    GENERAL_NOTIFICATION      // Genel bilgilendirme
+}
