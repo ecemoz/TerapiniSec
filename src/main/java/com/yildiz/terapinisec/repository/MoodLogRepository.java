05c4df2b850762228594020e25a346686f5c31a3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-28 18:40:19 +0300

    MoodLog Repository Finished.

--- src/main/java/com/yildiz/terapinisec/repository/MoodLogRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/MoodLogRepository.java
@@ -1,4 +1,19 @@
 package com.yildiz.terapinisec.repository;
 
-public class MoodLogRepository {
+import com.yildiz.terapinisec.model.MoodLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+
+@Repository
+public interface MoodLogRepository extends JpaRepository<MoodLog, Long> {
+
+    MoodLog findByMood(String mood);
+    MoodLog countByMood(String mood);
+    MoodLog findByUserId(Long userId);
+    MoodLog findByUserIdAndMood(Long userId, String mood);
+    MoodLog findByLogDateTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    MoodLog findByUserIdAndLogDateTimeBetween(Long userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
+    MoodLog countByUserIdAndMood(Long userId, String mood);
+    MoodLog findTopByUserIdOrderByMoodDesc(Long userId);
 }
--- src/main/java/com/yildiz/terapinisec/repository/TaskRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/TaskRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     Task findByTaskName(String taskName);
