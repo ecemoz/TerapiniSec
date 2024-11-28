91937ddea06630415189ec632b98448b0d3695f3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-28 19:06:33 +0300

    StoryView Repository  finished.

--- src/main/java/com/yildiz/terapinisec/repository/StoryViewRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/StoryViewRepository.java
@@ -1,4 +1,13 @@
 package com.yildiz.terapinisec.repository;
 
-public class StoryViewRepository {
+import com.yildiz.terapinisec.model.StoryView;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoryViewRepository extends JpaRepository<StoryView, Long> {
+
+    StoryView findByStoryId(Long storyId);
+    StoryView countByStoryId(Long storyId);
+    StoryView existsByUserIdAndStoryId(Long userId, Long storyId);
 }
