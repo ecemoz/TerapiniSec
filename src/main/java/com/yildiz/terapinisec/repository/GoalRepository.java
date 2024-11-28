b1cf99201c0280a380406a14df2629a573becab1
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-11-28 19:52:56 +0300

    Goal Repository finished.

--- src/main/java/com/yildiz/terapinisec/repository/GoalRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/GoalRepository.java
@@ -1,4 +1,16 @@
 package com.yildiz.terapinisec.repository;
 
-public class GoalRepository {
+import com.yildiz.terapinisec.model.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+    List<Goal>findByGoalType(String goalType);
+    List<Goal>findByIsCompletedTrue();
+    List<Goal>findByIsCompletedFalse();
+    List<Goal>findByGoalOwnerId(Long userId);
+    List<Goal>findByGoalOwnerIdAndIsCompletedFalse(Long userId);
 }
