50373078ddda1edaa4e1d0bff39f080f401716e8
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 13:11:29 +0300

    participant repository finished.

--- src/main/java/com/yildiz/terapinisec/repository/ParticipantRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/ParticipantRepository.java
@@ -1,4 +1,16 @@
 package com.yildiz.terapinisec.repository;
 
-public class ParticipantRepository {
+import com.yildiz.terapinisec.model.Participant;
+import com.yildiz.terapinisec.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<Participant,Long> {
+
+    List<Participant> findBySessionId(Long sessionId);
+    List<Participant>findByJoinedUserId(Long userId);
+    List<User>findJoinedUserBySessionId(Long sessionId);
+    long countBySessionId(Long sessionId);
 }
--- src/main/java/com/yildiz/terapinisec/repository/SessionRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/SessionRepository.java
@@ -4,7 +4,6 @@ import com.yildiz.terapinisec.model.Session;
 import com.yildiz.terapinisec.util.SessionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
