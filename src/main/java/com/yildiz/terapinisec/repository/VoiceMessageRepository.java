f3e0a0f2042caa993702326976badee2176fb865
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 16:38:50 +0300

    voice message repository finished.

--- src/main/java/com/yildiz/terapinisec/repository/VoiceMessageRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/VoiceMessageRepository.java
@@ -1,4 +1,15 @@
 package com.yildiz.terapinisec.repository;
 
-public class VoiceMessageRepository {
+import com.yildiz.terapinisec.model.VoiceMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface VoiceMessageRepository extends JpaRepository<VoiceMessage, Long> {
+
+    List<VoiceMessage>findBySpeakerId(Long senderId);
+    List<VoiceMessage>findByListenerId(Long recipientId);
+    List<VoiceMessage>findBySpeakerIdAndListenerId(Long speakerId, Long listenerId);
+    VoiceMessage findByAudioUrl(String audioUrl);
 }
