1c574eeee26dafee3e12d60e69e5d0cf37209e54
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 16:04:42 +0300

    meditation content repo finished.

---
+++ src/main/java/com/yildiz/terapinisec/repository/MeditationContentRepository.java
@@ -0,0 +1,16 @@
+package com.yildiz.terapinisec.repository;
+
+import com.yildiz.terapinisec.model.MeditationContent;
+import com.yildiz.terapinisec.util.MeditationSessionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MeditationContentRepository  extends JpaRepository<MeditationContent, Long> {
+
+    List<MeditationContent>findByIsPublicTrue();
+    List<MeditationContent>findByType(MeditationSessionType type);
+    List<MeditationContent>findByIsPublicFalse();
+    List<MeditationContent>findByCreatedById(Long userId);
+}
