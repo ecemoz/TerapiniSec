e14cdb0fcf6fdae471fa08816f4a094b178d8826
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 13:44:46 +0300

    survey response repository is finished.

--- src/main/java/com/yildiz/terapinisec/repository/SurveyResponseRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/SurveyResponseRepository.java
@@ -1,4 +1,15 @@
 package com.yildiz.terapinisec.repository;
 
-public class SurveyResponseRepository {
+import com.yildiz.terapinisec.model.SurveyResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface SurveyResponseRepository extends JpaRepository<SurveyResponse, Long> {
+
+    List<SurveyResponse>findByResponsedById(Long userId);
+    List<SurveyResponse>findBySurveyId(Long surveyId);
+    List<SurveyResponse>findByResponsedByIdAndSurveyId(Long userId, Long surveyId);
+    List<SurveyResponse>findBySurveyIdOrderBySubmittedDateDesc(Long surveyId);
 }
