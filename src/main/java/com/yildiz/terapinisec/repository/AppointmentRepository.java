884909e14e6c7c5bb1850e4b45fef895819ffe6f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 16:22:31 +0300

    appointment repo is finished.

--- src/main/java/com/yildiz/terapinisec/repository/AppointmentRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/AppointmentRepository.java
@@ -1,4 +1,20 @@
 package com.yildiz.terapinisec.repository;
 
-public class AppointmentRepository {
+import com.yildiz.terapinisec.model.Appointment;
+import com.yildiz.terapinisec.util.AppointmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment>findByAppointmentDateAfter(LocalDateTime appointmentDate);
+    Page<Appointment> findByAppointmentDateBetween(LocalDateTime appointmentDate1, LocalDateTime appointmentDate2, Pageable pageable);
+    List<Appointment>findByStatus(AppointmentStatus status);
+    Page<Appointment>findByAppointmentDateBetweenAndStatus(LocalDateTime appointmentDate1, LocalDateTime appointmentDate2, AppointmentStatus status, Pageable pageable);
+    List<Appointment>findByUserId(Long userId);
 }
