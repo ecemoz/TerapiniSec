fe3cf318c594e0a1c68813c1804406ee737127d9
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 14:05:35 +0300

    sleepLog Repository finished.

--- src/main/java/com/yildiz/terapinisec/repository/SleepLogRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/SleepLogRepository.java
@@ -1,4 +1,17 @@
 package com.yildiz.terapinisec.repository;
 
-public class SleepLogRepository {
+import com.yildiz.terapinisec.model.SleepLog;
+import com.yildiz.terapinisec.util.SleepQuality;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface SleepLogRepository extends JpaRepository<SleepLog, Long> {
+
+    List<SleepLog>findBySleeperId(Long id);
+    List<SleepLog>findBySleepDate(LocalDateTime sleepDate);
+    List<SleepLog>findBySleeperIdAndSleepDate(Long id, LocalDateTime sleepDate);
+    List<SleepLog>findBySleepQualityAndSleeperId(SleepQuality sleepQuality, Long id);
 }
