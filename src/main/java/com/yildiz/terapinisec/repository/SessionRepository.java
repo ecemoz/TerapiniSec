1df7c203a13c54f63170f408809a45bca732a80e
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-01 12:52:46 +0300

    Session Repository finished.

--- src/main/java/com/yildiz/terapinisec/repository/SessionRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/SessionRepository.java
@@ -1,4 +1,18 @@
 package com.yildiz.terapinisec.repository;
 
-public class SessionRepository {
+import com.yildiz.terapinisec.model.Session;
+import com.yildiz.terapinisec.util.SessionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+    List<Session>findBySessionType(SessionType sessionType);
+    List<Session>findBySessionDateTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+    //belli bir tarih girilecek ve o tarihin saat 00-23.59 arası sorgulama listesi döndürülecek servis katmanında...
+    List<Session>findBySessionNameContainingIgnoreCase(String keyword);
 }
