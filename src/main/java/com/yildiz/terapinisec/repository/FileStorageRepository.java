82cdaffd5fa4de3790e6e803bb0694e2e0f5e01d
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-02 13:35:38 +0300

    file storage repository  finished

--- src/main/java/com/yildiz/terapinisec/repository/FileStorageRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/FileStorageRepository.java
@@ -1,4 +1,23 @@
 package com.yildiz.terapinisec.repository;
 
-public class FileStorageRepository {
+import com.yildiz.terapinisec.model.FileStorage;
+import com.yildiz.terapinisec.util.FileType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface FileStorageRepository extends JpaRepository<FileStorage, Long> {
+
+    Page<FileStorage>findByUploaderId(Long uploaderId, Pageable pageable);
+    List<FileStorage> findByFileType(FileType fileType);
+    List<FileStorage>findByFileNameContainingOrDescriptionContaining(String fileNameKeyWord, Pageable pageable);
+    List<FileStorage>findByUploaderIdAndFileType(Long uploaderId, FileType fileType);
+    List<FileStorage>findByIsPublicTrue();
+    List<FileStorage>findByUploaderIdAndIsPublicFalse(Long uploaderId);
+    List<FileStorage>findByUploadDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    FileStorage findByFileName(String fileName);
 }
