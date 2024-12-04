b4d1d89e39477ffeac79fefeb5d6eed365967eee
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-04 20:29:33 +0300

    story scheduler fixed

--- src/main/java/com/yildiz/terapinisec/service/StoryService.java
+++ src/main/java/com/yildiz/terapinisec/service/StoryService.java
@@ -1,4 +1,53 @@
 package com.yildiz.terapinisec.service;
 
+import com.yildiz.terapinisec.model.Story;
+import com.yildiz.terapinisec.repository.StoryRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StoryService {
+
+    @Autowired
+    private StoryRepository storyRepository;
+
+    public List<Story>getAllStories(){
+        return storyRepository.findAll();
+    }
+
+    public Optional<Story> getStoryById(Long id){
+        return storyRepository.findById(id);
+    }
+
+    public Story createStory(Story story){
+        return storyRepository.save(story);
+    }
+
+    public Story updateStory(Long id, Story updatedStory){
+        return storyRepository.findById(id)
+                .map(story -> {
+                   story.setDescription(updatedStory.getDescription());
+                   return storyRepository.save(story);
+
+                })
+                .orElseThrow(()-> new RuntimeException("Story not found"));
+    }
+
+    @Transactional
+    public void deactivateOldStories() {
+        LocalDateTime cutoffTime = LocalDateTime.now().minusHours(24);
+        storyRepository.findAll().forEach(story -> {
+            if (story.isActive() && story.getStoryCreatedAt().isBefore(cutoffTime)) {
+                story.setActive(false);
+                storyRepository.save(story);
+            }
+        });
+    }
+
+
 }
--- src/main/java/com/yildiz/terapinisec/util/StoryScheduler.java
+++ src/main/java/com/yildiz/terapinisec/util/StoryScheduler.java
@@ -1,34 +1,20 @@
 package com.yildiz.terapinisec.util;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import com.yildiz.terapinisec.service.StoryService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import java.time.LocalDateTime;
-import com.yildiz.terapinisec.repository.StoryRepository;
 
 @Component
 public class StoryScheduler {
 
-    private final StoryRepository storyRepository;
+    private final StoryService storyService;
 
-    public StoryScheduler(StoryRepository storyRepository) {
-        this.storyRepository = storyRepository;
+    public StoryScheduler(StoryService storyService) {
+        this.storyService = storyService;
     }
 
     @Scheduled(fixedRate=3600000)
     public void updateStoryStatus() {
-        LocalDateTime now = LocalDateTime.now();
-        storyRepository.findAll().forEach(story -> {
-            if (story.isActive() && story.getCreatedAt().isBefore(now.minusHours(24))) {
-                story.setActive(false);
-                storyRepository.save(story);
-            }
-        });
-    }
-
-    @Configuration
-    @EnableScheduling
-    public class SchedulerConfig{
+        storyService.deactivateOldStories();
     }
 }
