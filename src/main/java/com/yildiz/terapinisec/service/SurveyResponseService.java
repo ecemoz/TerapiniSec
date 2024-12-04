10f62691320141e57e6239218ad0bb8adb8265b5
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-04 18:23:13 +0300

    surveyresponse service finished.

--- src/main/java/com/yildiz/terapinisec/service/SurveyResponseService.java
+++ src/main/java/com/yildiz/terapinisec/service/SurveyResponseService.java
@@ -1,4 +1,43 @@
 package com.yildiz.terapinisec.service;
 
+import com.yildiz.terapinisec.model.SurveyResponse;
+import com.yildiz.terapinisec.repository.SurveyResponseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SurveyResponseService {
+
+    @Autowired
+    private SurveyResponseRepository surveyResponseRepository;
+
+    public List<SurveyResponse>getAllSurveyResponses(){
+        return surveyResponseRepository.findAll();
+    }
+
+    public Optional<SurveyResponse> getSurveyResponseById(Long id){
+        return surveyResponseRepository.findById(id);
+    }
+
+    public SurveyResponse createSurveyResponse(SurveyResponse surveyResponse){
+       return surveyResponseRepository.save(surveyResponse);
+    }
+
+    public  List<SurveyResponse>findByResponsedById(Long userId){
+         return surveyResponseRepository.findByResponsedById(userId);
+    }
+
+    public List<SurveyResponse>findBySurveyId(Long surveyId){
+        return surveyResponseRepository.findBySurveyId(surveyId);
+    }
+
+    public List<SurveyResponse>findByResponsedByIdAndSurveyId(Long userId, Long surveyId){
+        return surveyResponseRepository.findByResponsedByIdAndSurveyId(userId, surveyId);
+    }
+
+    public List<SurveyResponse>findBySurveyIdOrderBySubmittedDateDesc(Long surveyId){
+        return surveyResponseRepository.findBySurveyIdOrderBySubmittedDateDesc(surveyId);
+    }
 }
