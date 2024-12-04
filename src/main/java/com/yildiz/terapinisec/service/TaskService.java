e99e3396a0184234adf556b8274eb0109a8f0c0b
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-04 17:30:11 +0300

    task service is fixed.

--- src/main/java/com/yildiz/terapinisec/service/TaskService.java
+++ src/main/java/com/yildiz/terapinisec/service/TaskService.java
@@ -4,9 +4,7 @@ import com.yildiz.terapinisec.model.Task;
 import com.yildiz.terapinisec.repository.TaskRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
