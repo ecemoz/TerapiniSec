9b1b7a50a45385cb666d7ef83d55e832b49a5d7a
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-08 20:28:09 +0300

    goal repository updated.

--- src/main/java/com/yildiz/terapinisec/repository/GoalRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/GoalRepository.java
@@ -2,7 +2,11 @@ package com.yildiz.terapinisec.repository;
 
 import com.yildiz.terapinisec.model.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +17,10 @@ public interface GoalRepository extends JpaRepository<Goal, Long> {
     List<Goal>findByIsCompletedFalse();
     List<Goal>findByGoalOwnerId(Long userId);
     List<Goal>findByGoalOwnerIdAndIsCompletedFalse(Long userId);
+
+    @Query("SELECT COUNT(g) FROM Goal g WHERE g.goalOwner.id = :userId AND g.goalComplete = true AND g.goalEndDate BETWEEN :startDate AND :endDate ")
+    long countCompletedGoalsByUserAndDateRange(@Param("userId") Long userId, @Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
+
+    @Query("SELECT COUNT(g) FROM Goal g WHERE g.goalOwner.id = :userId AND g.goalEndDate BETWEEN :startDate AND :endDate ")
+    long countGoalsByUserAndDateRange (@Param("userId") Long userId, @Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
 }
