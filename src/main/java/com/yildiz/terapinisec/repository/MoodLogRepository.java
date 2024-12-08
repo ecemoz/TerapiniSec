fa9f62d7f88b3030889472b6561e38aa4754a8be
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-08 20:02:46 +0300

    moodlog repository updated.

--- src/main/java/com/yildiz/terapinisec/repository/MoodLogRepository.java
+++ src/main/java/com/yildiz/terapinisec/repository/MoodLogRepository.java
@@ -2,6 +2,8 @@ package com.yildiz.terapinisec.repository;
 
 import com.yildiz.terapinisec.model.MoodLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,4 +19,7 @@ public interface MoodLogRepository extends JpaRepository<MoodLog, Long> {
     List<MoodLog> findByUserIdAndLogDateTimeBetween(Long userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
     long countByUserIdAndMood(Long userId, String mood);
     MoodLog findTopByUserIdOrderByMoodDesc(Long userId);
+
+    @Query("SELECT m FROM MoodLog m WHERE m.moodOwner.id = :userId AND m.logDateTime BETWEEN :startDate AND :endDate")
+    List<MoodLog> findMoodSummaryByUserAndDateRange(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
