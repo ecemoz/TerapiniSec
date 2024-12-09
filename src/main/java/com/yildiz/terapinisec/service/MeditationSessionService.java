8c681f0e7ec0615569fb5276ea7252a4eda80012
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-09 17:20:29 +0300

    meditation service finished.

--- src/main/java/com/yildiz/terapinisec/service/MeditationSessionService.java
+++ src/main/java/com/yildiz/terapinisec/service/MeditationSessionService.java
@@ -1,4 +1,44 @@
 package com.yildiz.terapinisec.service;
 
+import com.yildiz.terapinisec.model.MeditationSession;
+import com.yildiz.terapinisec.repository.MeditationSessionRepository;
+import com.yildiz.terapinisec.util.MeditationSessionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MeditationSessionService {
+
+    @Autowired
+    private MeditationSessionRepository meditationSessionRepository;
+
+    public List<MeditationSession> getAllMeditationSession() {
+        return meditationSessionRepository.findAll();
+    }
+
+    public Optional<MeditationSession> getMeditationSessionById(Long id) {
+        return meditationSessionRepository.findById(id);
+    }
+
+    public MeditationSession createMeditationSession(MeditationSession meditationSession) {
+        return meditationSessionRepository.save(meditationSession);
+    }
+
+    public  List<MeditationSession>findByMeditatorId(Long id) {
+        return meditationSessionRepository.findByMeditatorId(id);
+    }
+
+    public  int countByMeditationId (Long id) {
+        return meditationSessionRepository.countByMeditationId(id);
+    }
+
+    public List<MeditationSession>findByMeditationTitleContaining (String keyword) {
+        return meditationSessionRepository.findByMeditationTitleContaining(keyword);
+    }
+
+    public List<MeditationSession>findByMeditatorIdAndMeditationContentType(Long userId, MeditationSessionType type) {
+        return meditationSessionRepository.findByMeditatorIdAndMeditationContentType(userId, type);
+    }
 }
