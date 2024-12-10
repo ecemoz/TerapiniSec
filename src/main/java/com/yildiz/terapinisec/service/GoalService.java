045bf95ded8d6fcfad9057310ac2b3a3c41db18b
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2024-12-10 17:29:19 +0300

    goal service finished.

--- src/main/java/com/yildiz/terapinisec/service/GoalService.java
+++ src/main/java/com/yildiz/terapinisec/service/GoalService.java
@@ -1,4 +1,74 @@
 package com.yildiz.terapinisec.service;
 
+import com.yildiz.terapinisec.model.Goal;
+import com.yildiz.terapinisec.repository.GoalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class GoalService {
+
+    @Autowired
+    private GoalRepository goalRepository;
+
+    public List<Goal> getAllGoals() {
+        return goalRepository.findAll();
+    }
+
+    public Optional<Goal> getGoalById(Long id) {
+        return goalRepository.findById(id);
+    }
+
+    public Goal createGoal(Goal goal) {
+        goal.setGoalComplete(goal.isGoalComplete());
+        goal.setGoalDescription(goal.getGoalDescription());
+        goal.setGoalStartDate(goal.getGoalStartDate());
+        goal.setGoalEndDate(goal.getGoalEndDate());
+        goal.setGoalType(goal.getGoalType());
+        goal.setGoalOwner(goal.getGoalOwner());
+        return goalRepository.save(goal);
+    }
+
+    public Goal updateGoal(Long id , Goal updatedGoal) {
+        return goalRepository.findById(id)
+                .map(goal -> {
+                    goal.setGoalDescription(updatedGoal.getGoalDescription());
+                    goal.setGoalStartDate(updatedGoal.getGoalStartDate());
+                    goal.setGoalEndDate(updatedGoal.getGoalEndDate());
+                    goal.setGoalType(updatedGoal.getGoalType());
+                    goal.setGoalComplete(updatedGoal.isGoalComplete());
+                    return goalRepository.save(goal);
+                })
+                .orElseThrow(()-> new RuntimeException(" Goal not found"));
+    }
+
+    public void deleteGoal(Long id) {
+        if (goalRepository.existsById(id)) {
+            goalRepository.deleteById(id);
+        } else {
+            throw new RuntimeException(" Goal not found");
+        }
+    }
+
+    public List<Goal>findByGoalType(String goalType) {
+        return goalRepository.findByGoalType(goalType);
+    }
+
+    public List<Goal>findByIsCompletedTrue() {
+        return goalRepository.findByIsCompletedTrue();
+    }
+
+    public List<Goal>findByIsCompletedFalse() {
+        return goalRepository.findByIsCompletedFalse();
+    }
+
+    public List<Goal>findByGoalOwnerId(Long userId) {
+        return goalRepository.findByGoalOwnerId(userId);
+    }
+
+    public List<Goal>findByGoalOwnerIdAndIsCompletedFalse(Long userId) {
+        return goalRepository.findByGoalOwnerIdAndIsCompletedFalse(userId);
+    }
 }
