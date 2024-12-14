431b4c1444733aba99a4d2817bdd058f656967fb
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-14 15:32:19 +0300

    userdto  is updated.

--- src/main/java/com/yildiz/terapinisec/dto/UserDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/UserDto.java
@@ -13,6 +13,7 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
     private LocalDateTime birthday;
     private UserRole userRole;
     private Boolean isPremium;
