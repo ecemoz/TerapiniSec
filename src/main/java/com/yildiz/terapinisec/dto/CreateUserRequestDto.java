f9331527d7814173326f66ca86437f0f2272c9f6
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-14 15:08:25 +0300

    CreateUserRequestDto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/CreateUserRequestDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/CreateUserRequestDto.java
@@ -1,4 +1,18 @@
 package com.yildiz.terapinisec.dto;
 
+import com.yildiz.terapinisec.util.UserRole;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class CreateUserRequestDto {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime birthday;
+    private String phoneNumber;
+    private String password;
+    private UserRole userRole;
 }
