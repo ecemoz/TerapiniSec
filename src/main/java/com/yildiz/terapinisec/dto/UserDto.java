553f88ebc7522b0880676c129d3afe33c84e55cc
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-14 15:04:41 +0300

    userdto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/UserDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/UserDto.java
@@ -1,4 +1,22 @@
 package com.yildiz.terapinisec.dto;
 
+import com.yildiz.terapinisec.util.Specialization;
+import com.yildiz.terapinisec.util.UserRole;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class UserDto {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime birthday;
+    private UserRole userRole;
+    private Boolean isPremium;
+    private List<Specialization> specializations;
+    private Integer yearsOfExperience;
+    private List<LocalDateTime> availableTimes;
 }
