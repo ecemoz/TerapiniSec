7b404c8554ebeba05e1873a0b601c24e2683f990
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-14 15:24:41 +0300

    update user request dto is finished.

---
+++ src/main/java/com/yildiz/terapinisec/dto/UpdateUserRequestDto.java
@@ -0,0 +1,19 @@
+package com.yildiz.terapinisec.dto;
+
+import com.yildiz.terapinisec.util.Specialization;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class UpdateUserRequestDto {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime birthday;
+    private List<Specialization> specializations;
+    private Integer yearsOfExperience;
+    private List<LocalDateTime>availableTimes;
+}
