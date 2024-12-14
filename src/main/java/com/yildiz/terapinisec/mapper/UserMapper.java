9d072a9f68e6a969a6bdb443a4cac0b567cfbf50
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-14 17:17:19 +0300

    user mapper is finished.

--- src/main/java/com/yildiz/terapinisec/mapper/UserMapper.java
+++ src/main/java/com/yildiz/terapinisec/mapper/UserMapper.java
@@ -1,4 +1,29 @@
 package com.yildiz.terapinisec.mapper;
 
-public class UserMapper {
+import com.yildiz.terapinisec.dto.*;
+import com.yildiz.terapinisec.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring" )
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "password", ignore = true)
+    UserResponseDto touserResponseDto(User user);
+
+    User toUser(UserCreateDto userCreateDto);
+
+    void updateUserFromDto(UserUpdateDto userUpdateDto, @MappingTarget User user);
+
+    User toUser (UserLoginDto userLoginDto);
+
+    UserLoginDto toUserLoginDto (User user);
+
+    User toUser (UserDto userDto);
+
+    UserDto toUserDto (User user);
 }
