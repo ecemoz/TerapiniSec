15cfad2ea05d884e605ce0ede315015844d73c01
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-19 22:19:58 +0300

    survey response response dto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/SurveyResponseResponseDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/SurveyResponseResponseDto.java
@@ -1,4 +1,14 @@
 package com.yildiz.terapinisec.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class SurveyResponseResponseDto {
+
+    private Long id;
+    private String responses;
+    private LocalDateTime submittedDate;
+    private String respondedByUsername;
+    private Long surveyId;
 }
