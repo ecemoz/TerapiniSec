06202ff1aef603fb0fea3bd4d8c69c20bb071cf1
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-21 17:07:57 +0300

    story detailed response dto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/StoryDetailedResponseDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/StoryDetailedResponseDto.java
@@ -1,4 +1,19 @@
 package com.yildiz.terapinisec.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class StoryDetailedResponseDto {
+
+    private Long id;
+    private String storyUrl;
+    private String description;
+    private LocalDateTime storyCreatedAt;
+    private boolean isActive;
+    private int viewCount;
+
+    private List<StoryViewResponseDto> storyViews;
 }
