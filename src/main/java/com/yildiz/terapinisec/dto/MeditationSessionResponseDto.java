294218bcf447035a13d7a76a18537675ad0c3573
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-31 16:11:37 +0300

    Meditation Session Response Dto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/MeditationSessionResponseDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/MeditationSessionResponseDto.java
@@ -1,4 +1,16 @@
 package com.yildiz.terapinisec.dto;
 
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Data
 public class MeditationSessionResponseDto {
+
+    private Long id ;
+    private Duration meditationSessionDuration;
+    private LocalDateTime meditationDateTime;
+    private String meditatorUsername;
+    private String meditationContentTitle;
 }
