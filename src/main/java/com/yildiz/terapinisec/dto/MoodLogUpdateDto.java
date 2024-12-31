020e8cad50da3494518729afc814aefca016ec31
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-31 14:18:49 +0300

    User Update Dto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/MoodLogUpdateDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/MoodLogUpdateDto.java
@@ -2,15 +2,11 @@ package com.yildiz.terapinisec.dto;
 
 import com.yildiz.terapinisec.util.UserMoods;
 import lombok.Data;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-public class MoodLogResponseDto {
+public class MoodLogUpdateDto {
 
-    private Long id;
     private List<UserMoods> usermoods;
     private String description;
-    private LocalDateTime logDateTime;
-    private String moodOwnerUsername;
 }
