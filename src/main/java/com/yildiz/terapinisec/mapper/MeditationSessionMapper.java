cf10b6991f8bbf08da74f8045d80694ce1427dc1
Ecem Nur Özen <ecemnurozen03@gmail.com>
2024-12-31 16:34:56 +0300

    meditation session mapper is finished.

---
+++ src/main/java/com/yildiz/terapinisec/mapper/MeditationSessionMapper.java
@@ -0,0 +1,20 @@
+package com.yildiz.terapinisec.mapper;
+
+import com.yildiz.terapinisec.dto.MeditationSessionCreateDto;
+import com.yildiz.terapinisec.dto.MeditationSessionResponseDto;
+import com.yildiz.terapinisec.model.MeditationSession;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MeditationSessionMapper {
+
+    @Mapping(source = "meditatorId" , target = "meditator.id")
+    @Mapping(source = "meditationContentId" , target = "meditationContent.id" )
+    MeditationSession toMeditationSession(MeditationSessionCreateDto meditationSessionCreateDto);
+
+    @Mapping(source = "meditator.username" , target = "meditatorUsername")
+    @Mapping(source = "meditationContent.title" , target = "meditationContentTitle")
+    MeditationSessionResponseDto toMeditationSessionResponseDto(MeditationSession meditationSession);
+
+}
