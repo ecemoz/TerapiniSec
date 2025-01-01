39830a5a214c4355de2a77a49b3239bcf796c5a2
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-01 22:46:10 +0300

    library document service is finished.

--- src/main/java/com/yildiz/terapinisec/service/LibraryDocumentService.java
+++ src/main/java/com/yildiz/terapinisec/service/LibraryDocumentService.java
@@ -1,5 +1,9 @@
 package com.yildiz.terapinisec.service;
 
+import com.yildiz.terapinisec.dto.LibraryDocumentCreateDto;
+import com.yildiz.terapinisec.dto.LibraryDocumentResponseDto;
+import com.yildiz.terapinisec.dto.LibraryDocumentUpdateDto;
+import com.yildiz.terapinisec.mapper.LibraryDocumentMapper;
 import com.yildiz.terapinisec.model.LibraryDocument;
 import com.yildiz.terapinisec.repository.LibraryDocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LibraryDocumentService {
@@ -16,37 +20,34 @@ public class LibraryDocumentService {
     @Autowired
     private LibraryDocumentRepository libraryDocumentRepository;
 
-    public Page<LibraryDocument> getAllLibraryDocuments(int page, int size) {
+    @Autowired
+    private LibraryDocumentMapper libraryDocumentMapper;
+
+    public Page<LibraryDocumentResponseDto> getAllLibraryDocuments(int page, int size) {
         Pageable pageable = PageRequest.of(page,size);
-        return libraryDocumentRepository.findAll(pageable);
+        Page<LibraryDocument> documents = libraryDocumentRepository.findAll(pageable);
+        return documents.map(libraryDocumentMapper::toLibraryDocumentResponseDto);
     }
 
-    public Optional<LibraryDocument> getLibraryDocumentById(Long id) {
-        return libraryDocumentRepository.findById(id);
+    public LibraryDocumentResponseDto getLibraryDocumentById(Long id) {
+        LibraryDocument document = libraryDocumentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid library document"));
+        return libraryDocumentMapper.toLibraryDocumentResponseDto(document);
     }
 
-    public LibraryDocument createLibraryDocument(LibraryDocument libraryDocument) {
-        libraryDocument.setTitle(libraryDocument.getTitle());
-        libraryDocument.setDescription(libraryDocument.getDescription());
-        libraryDocument.setDocumentUrl(libraryDocument.getDocumentUrl());
-        libraryDocument.setDocumentUploadDate(libraryDocument.getDocumentUploadDate());
-        libraryDocument.setPublic(libraryDocument.isPublic());
-        libraryDocument.setAccesibleByPremiumOnly(libraryDocument.isAccesibleByPremiumOnly());
-        libraryDocument.setFileUploader(libraryDocument.getFileUploader());
-        return libraryDocumentRepository.save(libraryDocument);
+    public LibraryDocumentResponseDto createLibraryDocument(LibraryDocumentCreateDto libraryDocumentCreateDto) {
+        LibraryDocument libraryDocument = libraryDocumentMapper.toLibraryDocument(libraryDocumentCreateDto);
+        LibraryDocument savedDocument = libraryDocumentRepository.save(libraryDocument);
+        return libraryDocumentMapper.toLibraryDocumentResponseDto(savedDocument);
     }
 
-    public LibraryDocument updateLibraryDocument(Long id , LibraryDocument updatedLibraryDocument) {
-        return libraryDocumentRepository.findById(id)
-                .map(libraryDocument -> {
-                    libraryDocument.setTitle(updatedLibraryDocument.getTitle());
-                    libraryDocument.setDescription(updatedLibraryDocument.getDescription());
-                    libraryDocument.setDocumentUrl(updatedLibraryDocument.getDocumentUrl());
-                    libraryDocument.setDocumentUploadDate(updatedLibraryDocument.getDocumentUploadDate());
-                    libraryDocument.setPublic(updatedLibraryDocument.isPublic());
-                    return libraryDocumentRepository.save(libraryDocument);
-                })
-                .orElseThrow(() -> new RuntimeException("Document not found."));
+    public LibraryDocumentResponseDto updateLibraryDocument(Long id , LibraryDocumentUpdateDto libraryDocumentUpdateDto) {
+        LibraryDocument existingDocument = libraryDocumentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid library document"));
+
+        libraryDocumentMapper.updateLibraryDocumentFromDto(libraryDocumentUpdateDto, existingDocument);
+        LibraryDocument savedDocument = libraryDocumentRepository.save(existingDocument);
+        return libraryDocumentMapper.toLibraryDocumentResponseDto(savedDocument);
     }
 
     public void deleteLibraryDocument(Long id) {
@@ -57,27 +58,41 @@ public class LibraryDocumentService {
         }
     }
 
-    public Page<LibraryDocument>findByIsPublicTrue(Pageable pageable) {
-        return libraryDocumentRepository.findByIsPublicTrue(pageable);
+    public Page<LibraryDocumentResponseDto>findByIsPublicTrue(Pageable pageable) {
+        Page<LibraryDocument> documents = libraryDocumentRepository.findByIsPublicTrue(pageable);
+        return documents.map(libraryDocumentMapper::toLibraryDocumentResponseDto);
     }
 
-    public Page<LibraryDocument>findByUploaderId(Long uploaderId, Pageable pageable) {
-        return libraryDocumentRepository.findByUploaderId(uploaderId, pageable);
+    public Page<LibraryDocumentResponseDto>findByUploaderId(Long uploaderId, Pageable pageable) {
+        Page<LibraryDocument> documents = libraryDocumentRepository.findByUploaderId(uploaderId, pageable);
+        return documents.map(libraryDocumentMapper::toLibraryDocumentResponseDto);
     }
 
-    public List<LibraryDocument> findByUploaderIdAndIsPublicTrue(Long uploaderId) {
-        return libraryDocumentRepository.findByUploaderIdAndIsPublicTrue(uploaderId);
+    public List<LibraryDocumentResponseDto> findByUploaderIdAndIsPublicTrue(Long uploaderId) {
+        List<LibraryDocument> documents = libraryDocumentRepository.findByUploaderIdAndIsPublicTrue(uploaderId);
+        return documents.stream()
+                .map(libraryDocumentMapper::toLibraryDocumentResponseDto)
+                .collect(Collectors.toList());
     }
 
-    public List<LibraryDocument>findByTitleContainingOrDescriptionContaining(String titleKeyword, String descriptionKeyword) {
-        return libraryDocumentRepository.findByTitleContainingOrDescriptionContaining(titleKeyword, descriptionKeyword);
+    public List<LibraryDocumentResponseDto>findByTitleContainingOrDescriptionContaining(String titleKeyword, String descriptionKeyword) {
+       List<LibraryDocument> documents = libraryDocumentRepository.findByTitleContainingOrDescriptionContaining(titleKeyword, descriptionKeyword);
+       return documents.stream()
+               .map(libraryDocumentMapper::toLibraryDocumentResponseDto)
+               .collect(Collectors.toList());
     }
 
-    public List<LibraryDocument>findByAccesibleByPremiumOnlyTrue() {
-        return libraryDocumentRepository.findByAccesibleByPremiumOnlyTrue();
+    public List<LibraryDocumentResponseDto>findByAccesibleByPremiumOnlyTrue() {
+        List<LibraryDocument> documents = libraryDocumentRepository.findByAccesibleByPremiumOnlyTrue();
+        return documents.stream()
+                .map(libraryDocumentMapper::toLibraryDocumentResponseDto)
+                .collect(Collectors.toList());
     }
 
-    public List<LibraryDocument>findByUploaderIdAndAccessibleByPremiumOnlyTrue(Long uploaderId) {
-        return libraryDocumentRepository.findByUploaderIdAndAccessibleByPremiumOnlyTrue(uploaderId);
+    public List<LibraryDocumentResponseDto>findByUploaderIdAndAccessibleByPremiumOnlyTrue(Long uploaderId) {
+        List<LibraryDocument> documents = libraryDocumentRepository.findByUploaderIdAndAccessibleByPremiumOnlyTrue(uploaderId);
+        return documents.stream()
+                .map(libraryDocumentMapper::toLibraryDocumentResponseDto)
+                .collect(Collectors.toList());
     }
 }
