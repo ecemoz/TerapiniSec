245c6652737cb770cdf6b9700a25485488493a15
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-01 22:12:55 +0300

    Library document response dto is finished.

--- src/main/java/com/yildiz/terapinisec/dto/LibraryDocumentResponseDto.java
+++ src/main/java/com/yildiz/terapinisec/dto/LibraryDocumentResponseDto.java
@@ -1,14 +1,17 @@
 package com.yildiz.terapinisec.dto;
 
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
-public class LibraryDocumentCreateDto {
+public class LibraryDocumentResponseDto {
 
+    private Long id;
     private String title;
     private String description;
     private String documentUrl;
+    private LocalDateTime documentUploadDate;
     private boolean isPublic;
     private boolean accesibleByPremiumOnly;
-    private Long fileUploaderId;
+    private String fileUploaderUsername;
 }
