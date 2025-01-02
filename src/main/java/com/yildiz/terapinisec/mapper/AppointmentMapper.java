77065ecf73bb98462febe50406bff059b82d7a36
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-02 23:46:04 +0300

    update Appointment mapper

--- src/main/java/com/yildiz/terapinisec/mapper/AppointmentMapper.java
+++ src/main/java/com/yildiz/terapinisec/mapper/AppointmentMapper.java
@@ -3,10 +3,15 @@ package com.yildiz.terapinisec.mapper;
 import com.yildiz.terapinisec.dto.AppointmentCreateDto;
 import com.yildiz.terapinisec.dto.AppointmentResponseDto;
 import com.yildiz.terapinisec.dto.AppointmentUpdateDto;
+import com.yildiz.terapinisec.dto.FileStorageResponseDto;
 import com.yildiz.terapinisec.model.Appointment;
+import com.yildiz.terapinisec.model.FileStorage;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+
+import java.util.List;
 
 @Mapper
 public interface AppointmentMapper {
@@ -20,4 +25,7 @@ public interface AppointmentMapper {
     AppointmentResponseDto toAppointmentResponseDto(Appointment appointment);
 
     void updateAppointmentFromDto(AppointmentUpdateDto appointmentUpdateDto, @MappingTarget Appointment appointment);
+
+    @IterableMapping( elementTargetType = FileStorageResponseDto.class)
+    List<FileStorageResponseDto> toFileStorageResponseDtoList(List<FileStorage> fileStorageList);
 }
