32053a944f158f865df9e487ad70023c9304253a
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-02 23:21:44 +0300

    AppointmentCreateDto is finished.

---
+++ src/main/java/com/yildiz/terapinisec/dto/AppointmentCreateDto.java
@@ -0,0 +1,14 @@
+package com.yildiz.terapinisec.dto;
+
+import com.yildiz.terapinisec.util.AppointmentStatus;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class AppointmentCreateDto {
+
+    private LocalDateTime appointmentTime;
+    private Long appointmentClientId;
+    private Long threapistId;
+    private AppointmentStatus appointmentStatus = AppointmentStatus.PENDING;
+}
--- src/main/java/com/yildiz/terapinisec/dto/AppointmentDto.java
+++
@@ -1,4 +0,0 @@
-package com.yildiz.terapinisec.dto;
-
-public class AppointmentDto {
-}
