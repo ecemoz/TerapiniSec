87ee0cf145d5ebf2a29d5588250bde76bf6bafae
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-02 23:10:36 +0300

    update file storage mapper

--- src/main/java/com/yildiz/terapinisec/mapper/FileStorageMapper.java
+++ src/main/java/com/yildiz/terapinisec/mapper/FileStorageMapper.java
@@ -4,9 +4,12 @@ import com.yildiz.terapinisec.dto.FileStorageCreateDto;
 import com.yildiz.terapinisec.dto.FileStorageResponseDto;
 import com.yildiz.terapinisec.dto.FileStorageUpdateDto;
 import com.yildiz.terapinisec.model.FileStorage;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface FileStorageMapper {
@@ -16,6 +19,9 @@ public interface FileStorageMapper {
 
     @Mapping(source = "documentUploader.username" , target = "documentUploaderUsername")
     FileStorageResponseDto toFileStorageResponseDto(FileStorage fileStorage);
+
+    @IterableMapping(elementTargetType = FileStorageResponseDto.class)
+    List<FileStorageResponseDto> toFileStorageResponseDtoList(List<FileStorage> fileStorages);
 
     void updateFileStorageFromDto(FileStorageUpdateDto fileStorageUpdateDto , @MappingTarget FileStorage filestorage);
 }
