4b0d8566656696fdb7d00dcd6f8ea6ada02d20eb
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-03 21:34:35 +0300

    update voice message mapper

--- src/main/java/com/yildiz/terapinisec/mapper/VoiceMessageMapper.java
+++ src/main/java/com/yildiz/terapinisec/mapper/VoiceMessageMapper.java
@@ -3,8 +3,11 @@ package com.yildiz.terapinisec.mapper;
 import com.yildiz.terapinisec.dto.VoiceMessageCreateDto;
 import com.yildiz.terapinisec.dto.VoiceMessageResponseDto;
 import com.yildiz.terapinisec.model.VoiceMessage;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface VoiceMessageMapper {
@@ -15,4 +18,7 @@ public interface VoiceMessageMapper {
     @Mapping(source = "speaker.id" , target = "speakerId")
     @Mapping(source = "listener.id" , target = "listenerId")
     VoiceMessageResponseDto toResponseDto(VoiceMessage voiceMessage);
+
+    @IterableMapping(elementTargetType = VoiceMessageResponseDto.class)
+    List<VoiceMessageResponseDto> toVoiceMessageResponseDtoList(List<VoiceMessage> voiceMessages);
 }
