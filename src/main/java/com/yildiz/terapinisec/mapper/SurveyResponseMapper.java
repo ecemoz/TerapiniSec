f57b9f312de1814a958f3f9e448a1a232d7736f0
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-04 20:56:48 +0300

    update survey response mapper

--- src/main/java/com/yildiz/terapinisec/mapper/SurveyResponseMapper.java
+++ src/main/java/com/yildiz/terapinisec/mapper/SurveyResponseMapper.java
@@ -1,12 +1,11 @@
 package com.yildiz.terapinisec.mapper;
 
 import com.yildiz.terapinisec.dto.SurveyResponseCreateDto;
-import com.yildiz.terapinisec.dto.SurveyPostDto;
 import com.yildiz.terapinisec.dto.SurveyResponsePostDto;
 import com.yildiz.terapinisec.model.SurveyResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+;
 
 @Mapper(componentModel = "spring")
 public interface SurveyResponseMapper {
