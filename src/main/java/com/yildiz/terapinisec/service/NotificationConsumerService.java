fe99481d24fb1633e1a007f326697a91d9a74190
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-09 22:26:26 +0300

    notification consumer service is finished.

---
+++ src/main/java/com/yildiz/terapinisec/service/NotificationConsumerService.java
@@ -0,0 +1,19 @@
+package com.yildiz.terapinisec.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationConsumerService {
+
+    @KafkaListener(topics = "notifications", groupId = "notification-group" )
+    public void listenMessages(String message) {
+        System.out.println("Received Messages: " + message);
+
+        processMessage(message);
+    }
+
+    private void processMessage(String message) {
+        System.out.println("Processing Message: " + message);
+    }
+}
