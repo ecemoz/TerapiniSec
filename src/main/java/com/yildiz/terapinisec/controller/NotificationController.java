56c23ff70862928287a4a04f45c3095fada013ba
Ecem Nur Özen <ecemnurozen03@gmail.com>
2025-01-09 22:38:02 +0300

    notification controller is finished.

---
+++ src/main/java/com/yildiz/terapinisec/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package com.yildiz.terapinisec.controller;
+
+import com.yildiz.terapinisec.service.NotificationProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @Autowired
+    private NotificationProducerService notificationProducerService;
+
+    @PostMapping
+    public String sendNotification(@RequestParam String message) {
+        String topic = "notifications";
+        notificationProducerService.sendMessage(topic, message);
+        return "Message sent to Kafka topic '" + topic + "': " + message;
+    }
+}
