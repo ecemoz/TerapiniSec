38c4b20b222b7e05f7600dac74b7ce0b114eb15f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-19 19:38:12 +0300

    story view controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/StoryViewController.java
+++ src/main/java/com/yildiz/terapinisec/controller/StoryViewController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,16 +21,19 @@ public class StoryViewController {
     private StoryViewService storyViewService;
 
     @PostMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<StoryViewResponseDto> createStoryView (@PathVariable StoryViewCreateDto storyViewCreateDto) {
         return ResponseEntity.ok(storyViewService.createStoryView(storyViewCreateDto));
     }
 
     @GetMapping("/{userId}/{storyId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Boolean> hasUserViewedStory (@PathVariable Long storyId , @PathVariable Long userId) {
         return ResponseEntity.ok(storyViewService.hasUserViewedStory(storyId ,userId));
     }
 
     @PostMapping("/{storyId}/view/{userId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<StoryViewResponseDto> addStoryView (@PathVariable Long storyId, @PathVariable Long userId) {
         try {
             StoryViewResponseDto storyViewResponseDto = storyViewService.addStoryView(storyId, userId);
@@ -40,21 +44,25 @@ public class StoryViewController {
     }
 
     @GetMapping("/ViewCount")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<Long> getViewCount(@RequestParam Long storyId) {
         return ResponseEntity.ok(storyViewService.getViewCountForStory(storyId));
     }
 
     @GetMapping("/view/{storyId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<Page<StoryViewResponseDto>> getViewsForStory (@PathVariable Long storyId, @RequestParam Pageable pageable) {
         return ResponseEntity.ok(storyViewService.getViewsForStory(storyId,pageable));
     }
 
     @GetMapping("/view-user/{userId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<Page<StoryViewResponseDto>> getViewsForUser (@PathVariable Long userId, @RequestParam Pageable pageable) {
         return ResponseEntity.ok(storyViewService.getViewsForUser(userId,pageable));
     }
 
     @GetMapping("/byuserviews/{userId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST') or @securityService.isSelf(#userId)")
     public ResponseEntity<List<StoryViewDetailedDto>> getViewsByUserWithStories( @PathVariable Long userId) {
         return ResponseEntity.ok(storyViewService.getViewsByUserWithStories(userId));
     }
