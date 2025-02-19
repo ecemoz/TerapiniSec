affdc95ce9d6bf4408c39242663b0c35c80a8b1f
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-19 19:25:10 +0300

    survey controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/SurveyController.java
+++ src/main/java/com/yildiz/terapinisec/controller/SurveyController.java
@@ -6,6 +6,7 @@ import com.yildiz.terapinisec.dto.SurveyUpdateDto;
 import com.yildiz.terapinisec.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -17,37 +18,44 @@ public class SurveyController {
     SurveyService surveyService;
 
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<SurveyPostDto>> getAllSurveys() {
         return ResponseEntity.ok(surveyService.getAllSurveys());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isSelf(#id)")
     public ResponseEntity<SurveyPostDto> getSurveyById(@PathVariable  Long id) {
         return ResponseEntity.ok(surveyService.getSurveyById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'PSYCHOLOGIST')")
     public ResponseEntity<SurveyPostDto> createSurvey(@RequestBody SurveyCreateDto surveyCreateDto) {
         return ResponseEntity.ok(surveyService.createSurvey(surveyCreateDto));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or (@securityService.isSelf(#id) and hasRole('PSYCHOLOGIST'))")
     public ResponseEntity<SurveyPostDto> updateSurvey(@PathVariable Long id, @RequestBody SurveyUpdateDto surveyUpdateDto) {
         return ResponseEntity.ok(surveyService.updateSurvey(id, surveyUpdateDto));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or (@securityService.isSelf(#id) and hasRole('PSYCHOLOGIST'))")
     public ResponseEntity<Void> deleteSurvey(@PathVariable Long id) {
         surveyService.deleteSurvey(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/search")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<SurveyPostDto>> getSurveyTitleContaining(@RequestParam String title) {
         return ResponseEntity.ok(surveyService.findByTitleContaining(title));
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<SurveyPostDto>> getSurveyByUserId(@PathVariable Long userId) {
         return ResponseEntity.ok(surveyService.findSurveyByUserId(userId));
     }
