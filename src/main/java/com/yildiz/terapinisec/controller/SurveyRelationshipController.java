5f8c9ce8c5d042f85462311fea2a590e5483000d
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-19 18:54:46 +0300

    survey relationship controller security thing finished

--- src/main/java/com/yildiz/terapinisec/controller/SurveyRelationshipController.java
+++ src/main/java/com/yildiz/terapinisec/controller/SurveyRelationshipController.java
@@ -6,6 +6,7 @@ import com.yildiz.terapinisec.service.SurveyRelationshipService;
 import com.yildiz.terapinisec.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,16 +21,19 @@ public class SurveyRelationshipController {
    private SurveyService surveyService;
 
     @GetMapping("/surveyId")
+    @PreAuthorize("hasAnyRole('ADMIN', 'PSYCHOLOGIST') or @securityService.hasUserRespondedToSurvey(#surveyId)")
     public ResponseEntity<List<SurveyResponsePostDto>> getResponsesBySurvey(@PathVariable Long surveyId) {
         return ResponseEntity.ok(surveyRelationshipService.getResponsesBySurvey(surveyId));
     }
 
     @PostMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<SurveyResponsePostDto> addResponseToSurvey(@PathVariable Long surveyId, @RequestBody SurveyResponseCreateDto surveyResponseCreateDto) {
         return ResponseEntity.ok(surveyRelationshipService.addResponseToSurvey(surveyId, surveyResponseCreateDto));
     }
 
     @DeleteMapping("/{surveyId}/with-responses")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteSurveyWithResponses(@PathVariable Long surveyId) {
         surveyRelationshipService.deleteSurveyWithResponses(surveyId);
         return ResponseEntity.noContent().build();
