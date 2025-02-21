8aad172a60de842def5951b402c9d97e24a64cd7
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-21 21:57:27 +0300

    Notification Controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/NotificationController.java
+++ src/main/java/com/yildiz/terapinisec/controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.yildiz.terapinisec.controller;
 
 import com.yildiz.terapinisec.service.NotificationProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ public class NotificationController {
     private NotificationProducerService notificationProducerService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public String sendNotification(@RequestParam String message) {
         String topic = "notifications";
         notificationProducerService.sendMessage(topic, message);
