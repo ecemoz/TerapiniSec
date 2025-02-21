97deea5f482878924ca0aa3a72b8b3947d698d87
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-21 21:54:09 +0300

    Participant Controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/ParticipantController.java
+++ src/main/java/com/yildiz/terapinisec/controller/ParticipantController.java
@@ -5,6 +5,7 @@ import com.yildiz.terapinisec.model.User;
 import com.yildiz.terapinisec.service.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -16,36 +17,37 @@ public class ParticipantController {
     private ParticipantService participantService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<ParticipantDto>> getAllParticipants() {
         return ResponseEntity.ok(participantService.getAllParticipants());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isSelf(#id)")
     public ResponseEntity<ParticipantDto> getParticipantById(@PathVariable Long id) {
         return ResponseEntity.ok(participantService.getParticipantById(id));
     }
 
-    @PostMapping
-    public ResponseEntity<ParticipantDto> createParticipant(@RequestBody ParticipantDto participantDto) {
-        return ResponseEntity.ok(participantService.createParticipant(participantDto));
-    }
-
     @GetMapping("/{sessionId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')or @securityService.isSessionParticipant(#sessionId)")
     public ResponseEntity<List<ParticipantDto>> getParticipantBySessionId(@PathVariable Long sessionId) {
         return ResponseEntity.ok(participantService.findBySessionId(sessionId));
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST') or @securityService.isSelf(#userId)")
     public ResponseEntity<List<ParticipantDto>> getParticipantByJoinedUser(@PathVariable Long userId) {
         return ResponseEntity.ok(participantService.findByJoinedUserId(userId));
     }
 
     @GetMapping("/joinedUser/{sessionId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST') or @securityService.isSessionParticipant(#sessionId)")
     public ResponseEntity<List<User>> findJoinedUserBySessionId(@PathVariable Long sessionId) {
         return ResponseEntity.ok(participantService.findJoinedUserBySessionId(sessionId));
     }
 
     @GetMapping("/count/{sessionId}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST') or @securityService.isSessionParticipant(#sessionId)")
     public ResponseEntity<Long> countBySessionId(@PathVariable Long sessionId) {
         return ResponseEntity.ok(participantService.countBySessionId(sessionId));
     }
