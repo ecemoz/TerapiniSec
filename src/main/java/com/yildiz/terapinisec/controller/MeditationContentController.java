52c23ec548b226ac66a6eb60edf66c14a471b172
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-23 18:56:52 +0300

    MeditationContent Controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/MeditationContentController.java
+++ src/main/java/com/yildiz/terapinisec/controller/MeditationContentController.java
@@ -7,6 +7,7 @@ import com.yildiz.terapinisec.service.MeditationContentService;
 import com.yildiz.terapinisec.util.MeditationSessionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -18,47 +19,56 @@ public class MeditationContentController {
     private MeditationContentService meditationContentService;
 
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<MeditationContentResponseDto>> getAllMeditationContent() {
         return ResponseEntity.ok(meditationContentService.getAllMeditationContent());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<MeditationContentResponseDto> getMeditationContentById(@PathVariable Long id) {
         return ResponseEntity.ok(meditationContentService.getMeditationContentById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<MeditationContentResponseDto> createMeditationContent(@RequestBody MeditationContentCreateDto meditationContentCreateDto) {
         return ResponseEntity.ok(meditationContentService.createMeditationContent(meditationContentCreateDto));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<MeditationContentResponseDto> updateMeditationContent(@PathVariable Long id, @RequestBody MeditationContentUpdateDto meditationContentUpdateDto) {
         return ResponseEntity.ok(meditationContentService.updateMeditationContent(id, meditationContentUpdateDto));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('PSYCHOLOGIST')")
     public ResponseEntity<MeditationContentResponseDto> deleteMeditationContent(@PathVariable Long id) {
         meditationContentService.deleteMeditationContentById(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/public-true")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<MeditationContentResponseDto>> getPublicMeditationContentByPublicTrue() {
         return ResponseEntity.ok(meditationContentService.findByIsPublicTrue());
     }
 
     @GetMapping("/public-false")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<MeditationContentResponseDto>> getPublicMeditationContentByIsPublicFalse() {
         return ResponseEntity.ok(meditationContentService.findByIsPublicFalse());
     }
 
     @GetMapping("/type")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<MeditationContentResponseDto>> getMeditationContentByType(@RequestParam MeditationSessionType sessionType) {
         return ResponseEntity.ok(meditationContentService.findByType(sessionType));
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<MeditationContentResponseDto>> getMeditationCreatedById(@PathVariable Long userId) {
         return ResponseEntity.ok(meditationContentService.findByCreatedById(userId));
     }
