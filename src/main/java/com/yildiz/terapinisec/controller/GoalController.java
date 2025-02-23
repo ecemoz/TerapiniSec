8dd8b45358db44730b2ea78e5e53b5ad8d3f5259
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-23 19:25:39 +0300

    Goal Controller security thing is finished.

--- src/main/java/com/yildiz/terapinisec/controller/GoalController.java
+++ src/main/java/com/yildiz/terapinisec/controller/GoalController.java
@@ -7,6 +7,7 @@ import com.yildiz.terapinisec.service.GoalService;
 import com.yildiz.terapinisec.util.GoalType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -18,52 +19,62 @@ public class GoalController {
     private GoalService goalService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<GoalResponseDto>> getAllGoals() {
         return ResponseEntity.ok(goalService.getAllGoals());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isGoalOwner(#id)")
     public ResponseEntity<GoalResponseDto> getGoalById(@PathVariable Long id) {
         return ResponseEntity.ok(goalService.getGoalById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     public ResponseEntity<GoalResponseDto> createGoal(@RequestBody GoalCreateDto goalCreateDto) {
         return ResponseEntity.ok(goalService.createGoal(goalCreateDto));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isGoalOwner(#id)")
     public ResponseEntity<GoalResponseDto> updateGoal(@PathVariable Long id, @RequestBody GoalUpdateDto goalUpdateDto) {
         return ResponseEntity.ok(goalService.updateGoal(id, goalUpdateDto));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isGoalOwner(#id)")
     public ResponseEntity<GoalResponseDto> deleteGoal(@PathVariable Long id) {
         goalService.deleteGoal(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/goaltype")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<GoalResponseDto>> getGoalByGoalType(@RequestParam GoalType goalType) {
         return ResponseEntity.ok(goalService.findByGoalType(goalType));
     }
 
     @GetMapping("/completed-true")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isGoalOwner(#id)")
     public ResponseEntity<List<GoalResponseDto>> getGoalCompletedTrue() {
         return ResponseEntity.ok(goalService.findByIsCompletedTrue());
     }
 
     @GetMapping("/completed-false")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isGoalOwner(#id)")
     public ResponseEntity<List<GoalResponseDto>> getGoalCompletedFalse() {
         return ResponseEntity.ok(goalService.findByIsCompletedFalse());
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isSelf(#userId)")
     public ResponseEntity<List<GoalResponseDto>> getGoalByGoalOwnerId(@PathVariable Long userId) {
         return ResponseEntity.ok(goalService.findByGoalOwnerId(userId));
     }
 
     @GetMapping("/{userId}/completed-false")
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isSelf(#userId)")
     public ResponseEntity<List<GoalResponseDto>> getGoalCompletedFalseByGoalOwnerId(@PathVariable Long userId) {
         return ResponseEntity.ok(goalService.findByGoalOwnerIdAndIsCompletedFalse(userId));
     }
