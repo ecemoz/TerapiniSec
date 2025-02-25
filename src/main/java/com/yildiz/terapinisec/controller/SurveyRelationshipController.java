8018c2b97fe9d8b4ffd19d17bb62ee03f7f39836
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-02-25 13:52:23 +0300

    survey relationship controller is updated.

--- src/main/java/com/yildiz/terapinisec/controller/SurveyRelationshipController.java
+++ src/main/java/com/yildiz/terapinisec/controller/SurveyRelationshipController.java
@@ -2,10 +2,7 @@ package com.yildiz.terapinisec.controller;
 
 import com.yildiz.terapinisec.dto.SurveyResponseCreateDto;
 import com.yildiz.terapinisec.dto.SurveyResponsePostDto;
-import com.yildiz.terapinisec.security.SecurityService;
 import com.yildiz.terapinisec.service.SurveyRelationshipService;
-import com.yildiz.terapinisec.service.SurveyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,22 +12,18 @@ import java.util.List;
 @RequestMapping("/surveyrelationship")
 public class SurveyRelationshipController {
 
-   @Autowired
-   private SurveyRelationshipService surveyRelationshipService;
+   private final  SurveyRelationshipService surveyRelationshipService;
+   public SurveyRelationshipController(SurveyRelationshipService surveyRelationshipService) {
+       this.surveyRelationshipService = surveyRelationshipService;
+   }
 
-   @Autowired
-   private SurveyService surveyService;
-
-    @Autowired
-    private SecurityService securityService;
-
-    @GetMapping("/surveyId")
+    @GetMapping("/{surveyId}")
     @PreAuthorize("hasAnyRole('ADMIN', 'PSYCHOLOGIST') or @securityService.hasUserRespondedToSurvey(#surveyId)")
     public ResponseEntity<List<SurveyResponsePostDto>> getResponsesBySurvey(@PathVariable Long surveyId) {
         return ResponseEntity.ok(surveyRelationshipService.getResponsesBySurvey(surveyId));
     }
 
-    @PostMapping
+    @PostMapping("/{surveyId}")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<SurveyResponsePostDto> addResponseToSurvey(@PathVariable Long surveyId, @RequestBody SurveyResponseCreateDto surveyResponseCreateDto) {
         return ResponseEntity.ok(surveyRelationshipService.addResponseToSurvey(surveyId, surveyResponseCreateDto));
